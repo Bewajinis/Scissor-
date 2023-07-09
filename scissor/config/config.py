b68b90372f3a802c6dd0165866278e293fb4d808
Emmanuel Bewaji <101416210+Bewajinis@users.noreply.github.com>
2023-07-09 21:38:51 +0100

    Add files via upload

--- scissor/config/config.py
+++ scissor/config/config.py
@@ -2,7 +2,7 @@ import os
 from decouple import config
 from datetime import timedelta
 
-class config:
+class Config:
     SECRET_KEY=config('SECRET_KEY', 'secret')
     JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=30)
     JWT_REFRESH_TOKEN_EXPIRES=timedelta(minutes=30)
@@ -10,22 +10,30 @@ class config:
 
 BASE_DIR=os.path.dirname(os.path.realpath(__file__))
 
-class DevConfig(config):
-    DEBUG=config()
+class DevConfig(Config):
+    DEBUG=True
     SQLALCHEMY_ECHO=True
     SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(BASE_DIR,'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
     
 
-class TestConfig(config):
-    pass
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
-class ProdConfig(config):
-    pass
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = config('DEBUG', False, cast=bool)
 
 config_dict={
     'dev':DevConfig,
     'prod':ProdConfig,
     'test':TestConfig
 }
+
+#postgres://sciurl:snhSnn99yPOH1pufMKj8YS0fEXC4WKpS@dpg-cilfkdtgkuvinfkmm0a0-a.oregon-postgres.render.com/sciss_bl8n
